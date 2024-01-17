05e1e2781f91cc2dc94d521c013998c73f3ff20e
vijay262000 <121922341+vijay262000@users.noreply.github.com>
2024-01-17 20:36:42 +0530

    Add files via upload

---
+++ dowhile.php
@@ -0,0 +1,15 @@
+<?php
+echo "this is do while loop in php<br>";
+/*do{
+    some linesw of code
+}while(condition);
+*/
+$i=0;
+do {
+    echo"sometimes $i <br>";
+    $i++;
+} while($i<5);
+
+
+
+?>
